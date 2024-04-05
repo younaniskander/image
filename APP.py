d975f0818fc0e2bba2239b4e1631bd07b99e617e
Younan Iskander <87044703+younaniskander@users.noreply.github.com>
2024-04-05 02:19:52 +0200

    APP.py

--- APP.py
+++ APP.py
@@ -2,36 +2,25 @@ import streamlit as st
 import tensorflow as tf
 from PIL import Image
 import numpy as np
-import requests
-from io import BytesIO
 import os
 
-# Define a function to download the model
-def download_model(model_url, model_path):
-    if not os.path.exists(model_path):
-        st.write("Downloading model...")
-        response = requests.get(model_url)
-        response.raise_for_status()
-        with open(model_path, "wb") as f:
-            f.write(response.content)
-        st.write("Model downloaded successfully.")
-    else:
-        st.write("Model already exists.")
-
-# Specify the model's URL and local path
-model_url = 'https://drive.google.com/file/d/13VwABEDa1Wchyg0n_RmZUMKsVcrDMWM9/view?usp=sharing'
-model_path = 'model_2021_2D_UNet.h5'
-
-# Download the model
-download_model(model_url, model_path)
-
-# Load your trained model
-model = tf.keras.models.load_model(model_path, 
-                                    custom_objects={'accuracy' : tf.keras.metrics.MeanIoU(num_classes=4),
-                                                    "precision": precision,
-                                                    "sensitivity":sensitivity,
-                                                    "specificity":specificity,
-                                                    }, compile=False)
+# Function to load a model uploaded by the user
+def load_model(uploaded_model):
+    if uploaded_model is not None:
+        # To read the model file, we need to write it to a temporary file then load it
+        with open("model_UNet.h5", "wb") as f:
+            f.write(uploaded_model.getbuffer())
+        return tf.keras.models.load_model("model_UNet.h5", 
+                                          custom_objects={'accuracy': tf.keras.metrics.MeanIoU(num_classes=4),
+                                                          "dice_coef": dice_coef,
+                                                          "precision": precision,
+                                                          "sensitivity": sensitivity,
+                                                          "specificity": specificity,
+                                                          # "dice_coef_necrotic": dice_coef_necrotic,
+                                                          # "dice_coef_edema": dice_coef_edema,
+                                                          # "dice_coef_enhancing": dice_coef_enhancing
+                                                         }, compile=False)
+    return None
 
 # Function to preprocess the image
 def preprocess_image(image, image_width=128, image_height=128): # Adjust image_width and image_height as needed
@@ -42,24 +31,34 @@ def preprocess_image(image, image_width=128, image_height=128): # Adjust image_w
     return image
 
 # Function to make predictions
-def make_prediction(image):
-    preprocessed_img = preprocess_image(image)
-    prediction = model.predict(preprocessed_img)
-    return prediction
+def make_prediction(model, image):
+    if model and image is not None:
+        preprocessed_img = preprocess_image(image)
+        prediction = model.predict(preprocessed_img)
+        return prediction
+    return None
 
 # Streamlit app
 def main():
     st.title("Image Classification with your Model")
 
-    uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+    # Upload model
+    uploaded_model = st.file_uploader("Upload a model", type=["h5"], key="model")
+    model = load_model(uploaded_model)
+
+    # Upload image
+    uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"], key="image")
 
     if uploaded_file is not None:
         image = Image.open(uploaded_file)
         st.image(image, caption='Uploaded Image.', use_column_width=True)
 
         if st.button('Make Prediction'):
-            prediction = make_prediction(image)
-            st.write("Prediction:", prediction)
+            if model is not None:
+                prediction = make_prediction(model, image)
+                st.write("Prediction:", prediction)
+            else:
+                st.error("Please upload a model to make predictions.")
 
 if __name__ == '__main__':
     main()
